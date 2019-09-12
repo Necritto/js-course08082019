7811f00b52bc77c24885078c25750a63a7ba1120
Necritto <onetopyr@mail.ru>
2019-09-12 11:02:54 +0300

    task  complete

--- closures/sort-by-field/script.js
+++ closures/sort-by-field/script.js
@@ -4,11 +4,11 @@ let users = [
     { name: "Ann", age: 19, surname: "Hathaway" }
 ];
 
-alert(users.sort(byField('name')));
-alert(users.sort(byField('age')));
+console.log(users.sort(byField('name')));
+console.log(users.sort(byField('age')));
 
 function byField(fieldName) {
     return (a, b) => {
-        return ( a.fieldName > b.fieldName ? 1 : -1);
-    };
+        return ( a[fieldName] > b[fieldName] ? 1 : -1);
+    }
 }
