8c99f8943675f5caf9ee214afe8751a1b346cc75
Necritto <onetopyr@mail.ru>
2019-09-15 09:45:16 +0300

    counter worked

--- DOM/tree-info/script.js
+++ DOM/tree-info/script.js
@@ -1,11 +1,7 @@
 'use strict';
 
 const li = document.querySelectorAll('li');
-const ul = document.querySelectorAll('ul');
-let countLi = 0;
 
-
-for (let data of li) { 
-    alert(`${data.firstChild.textContent} : ${countLi}`)
+for (let value of li) {
+  alert(`${value.firstChild.data.trim()} : ${value.querySelectorAll('li').length}`)
 }
-
