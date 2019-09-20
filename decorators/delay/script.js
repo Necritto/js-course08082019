d7fb9321f557e0b86ab0e5ba3d0946b731b241f4
Necritto <onetopyr@mail.ru>
2019-09-20 20:00:32 +0300

    delay has done

---
+++ decorators/delay/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 1500);
+
+f1000("test");
+f1500("test");
+
+function f(x) {
+  alert(x);
+}
+
+function delay(f, timeout) {
+  return function (...args) {
+
+    let timer = setTimeout(() => {
+      const result = f.call(this, ...args);
+      return result;
+    }, timeout);
+
+  }
+}
