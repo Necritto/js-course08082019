9a6ecd57cc92f05ffd68410cca89975ed7b13392
Necritto <onetopyr@mail.ru>
2019-09-27 10:46:35 +0300

    problem with voter prototype

---
+++ widgets-structure/voter-add-step/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+function Voter(options) {
+  var elem = this._elem = options.elem;
+  this._voteElem = elem.querySelector('.vote');
+
+  elem.onmousedown = function() {
+    return false;
+  };
+
+  elem.onclick = this._onClick.bind(this);
+}
+
+Voter.prototype._onClick = function(event) {
+  if (event.target.closest('.down')) {
+    this._voteDecrease();
+  } else if (event.target.closest('.up')) {
+    this._voteIncrease();
+  }
+};
+
+Voter.prototype._voteIncrease = function() {
+  this._voteElem.innerHTML = +this._voteElem.innerHTML + 1;
+};
+
+Voter.prototype._voteDecrease = function() {
+  this._voteElem.innerHTML = +this._voteElem.innerHTML - 1;
+};
+
+Voter.prototype.setVote = function(vote) {
+  this._voteElem.innerHTML = +vote;
+};
+
+function StepVoter(options) {
+  Voter.apply(this, arguments);
+  this._step = options.step || 1;
+}
+
+StepVoter.prototype = Object.create(Voter.prototype);
+
+StepVoter.prototype = function _voteDecrease() {
+  this._voteElem.innerHTML = +this._voteElem.innerHTML - this._step;
+}; 
+
+StepVoter.prototype = function _voteIncrease() {
+  this._voteElem.innerHTML = +this._voteElem.innerHTML + this._step;
+}; 
+
+let voter = new StepVoter({
+  elem: document.querySelector('.voter'),
+  step: 2
+});
+
