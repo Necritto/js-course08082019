f3a1c824358a08867c29e4e423d1a869b96a95c4
Necritto <onetopyr@mail.ru>
2019-10-01 09:09:42 +0300

    carousel v2.0 implemented

--- DOM/events/carousel/script.js
+++ DOM/events/carousel/script.js
@@ -1,34 +1,25 @@
 'use strict';
 
-let i = 1;
-for (let li of document.querySelectorAll('li')) {
-  li.style.position = 'relative';
-  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-  i++;
-}
-
 const previous = document.querySelector('.previous'),
   next = document.querySelector('.next'),
-  img = document.querySelectorAll('img');
-let index = 0;
+  ul = document.querySelector('ul'),
+  li = document.querySelectorAll('li');
+
+let width = 130,
+  count = 3,
+  position = 0;
 
 previous.addEventListener('click', previousSlide);
 next.addEventListener('click', nextSlide);
 
 function previousSlide() {
-  img[index].style.display = 'none';
-  index--;
-  if(index <= 0) {
-    index = img.length - 1;
-  }
-  img[index].style.display = 'block';
+  position += width * count;
+  position = Math.min(position, 0);
+  ul.style.marginLeft = position + 'px';
 }
 
 function nextSlide() {
-  img[index].style.display = 'none';
-  index++;
-  if(index >= img.length) {
-    index = 0;
-  }
-  img[index].style.display = 'block';
+  position -= width * count;
+  position = Math.max(position, -width * (li.length - count));
+  ul.style.marginLeft = position + 'px';
 }
