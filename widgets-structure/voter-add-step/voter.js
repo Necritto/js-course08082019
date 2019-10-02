fac2648178fe96b4ab099f4f2de525e2682db3ad
Necritto <onetopyr@mail.ru>
2019-10-02 20:53:11 +0300

    voter in es5 implemented

---
+++ widgets-structure/voter-add-step/voter.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function Voter(options) {
+  let elem = this._elem = options.elem;
+  this._voteElem = elem.querySelector('.vote');
+
+  elem.onclick = this._onClick.bind(this);
+}
+
+Voter.prototype._onClick = function(event) {
+  if (event.target.closest('.down')) {
+    this._voteDecrease();
+  } else if (event.target.closest('.up')) {
+    this._voteIncrease();
+  }
+};
+
+Voter.prototype._voteIncrease = function() {
+  this._voteElem.innerHTML = +this._voteElem.innerHTML + 1;
+};
+
+Voter.prototype._voteDecrease = function() {
+  this._voteElem.innerHTML = +this._voteElem.innerHTML - 1;
+};
+
+Voter.prototype.setVote = function(vote) {
+  this._voteElem.innerHTML = +vote;
+};
