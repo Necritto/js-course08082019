513e8332ac43585c3675ed7660ce2c52fffea085
Necritto <onetopyr@mail.ru>
2019-10-20 22:23:39 +0300

    chain-calls implemented

---
+++ object/chain-calls/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    return alert( this.step );
+  }
+};
+
+ladder
+      .up()
+      .up()
+      .down()
+      .showStep();
