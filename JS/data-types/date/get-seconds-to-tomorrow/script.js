4f7bc3b0f94f302644047ee60b8958f8ab41ea73
Necritto <onetopyr@mail.ru>
2019-10-30 22:18:29 +0300

    rest of seconds received

---
+++ JS/data-types/date/get-seconds-to-tomorrow/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log(getSecondsToTomorrow());
+
+function getSecondsToTomorrow() {
+  let today = new Date();
+  let tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+
+  return Math.round((tomorrow - today) / 1000);
+}
