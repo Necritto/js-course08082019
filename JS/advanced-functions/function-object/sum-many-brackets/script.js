03a8a7f12056e7e51b4453cb266cfca88396d96c
Necritto <onetopyr@mail.ru>
2019-11-02 12:39:24 +0300

    task implemented

---
+++ JS/advanced-functions/function-object/sum-many-brackets/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+alert(sum(1)(2));
+alert(sum(5)(-1)(2));
+alert(sum(6)(-1)(-2)(-3));
+alert(sum(0)(1)(2)(3)(4)(5));
+
+function sum(a) {
+
+  let currentSum = a;
+
+  function fn(b) {
+    currentSum += b;
+    return fn;
+  }
+
+  fn.toString = function () {
+    return currentSum;
+  };
+
+  return fn;
+}
