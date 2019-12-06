5e0b1ae1f43d5185abbd0d3980aa5eb83f6786e9
Necritto <onetopyr@mail.ru>
2019-12-06 18:13:29 +0300

    callback implemented

---
+++ JS/async/callbacks/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+document.body.insertAdjacentHTML('beforebegin', '<button class="btn">Click me</button>');
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', go);
+
+function go() {
+  showCircle(150, 150, 100, div => {
+    div.classList.add('message-ball');
+    div.append('Hello, world!');
+  });
+}
+
+function showCircle(cx, cy, radius, callback) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.classList.add('circle');
+  document.body.append(div);
+
+  setTimeout(() => {
+    div.style.width = radius * 2 + 'px';
+    div.style.height = radius * 2 + 'px';
+
+    div.addEventListener('transitionend', function handler() {
+      div.removeEventListener('transitionend', handler);
+      callback(div);
+    });
+  });
+}
