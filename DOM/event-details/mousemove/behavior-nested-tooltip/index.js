6fdb3f60154bd09cc8436b3192adc9052c0c7303
Necritto <onetopyr@mail.ru>
2019-12-28 14:36:49 +0300

    task implemented

---
+++ DOM/event-details/mousemove/behavior-nested-tooltip/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let tooltip;
+
+document.addEventListener('mouseover', function (event) {
+  let anchorElem = event.target.closest('[data-tooltip]');
+
+  if (!anchorElem) return;
+
+  tooltip = showTooltip(anchorElem, anchorElem.dataset.tooltip);
+});
+
+document.addEventListener('mouseout', function () {
+  if (tooltip) {
+    tooltip.remove();
+    tooltip = false;
+  }
+});
+
+
+function showTooltip(anchorElem, html) {
+  let tooltipElem = document.createElement('div');
+
+  tooltipElem.classList.add('tooltip');
+  tooltipElem.innerHTML = html;
+  document.body.append(tooltipElem);
+
+  let coords = anchorElem.getBoundingClientRect();
+
+  let left = coords.left + (anchorElem.offsetWidth - tooltipElem.offsetWidth) / 2;
+  if (left < 0) left = 0;
+
+  let top = coords.top - tooltipElem.offsetHeight - 5;
+  if (top < 0) {
+    top = coords.top + anchorElem.offsetHeight + 5;
+  }
+
+  tooltipElem.style.left = left + 'px';
+  tooltipElem.style.top = top + 'px';
+
+  return tooltipElem;
+}
